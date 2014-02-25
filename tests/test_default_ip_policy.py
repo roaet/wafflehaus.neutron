c177b96eec80e58065921508bad3ddd80b4c946c
jmeridth <jmeridth@gmail.com>
2014-02-25 03:51:41 +0000

    Add functionality to handle setting default allocation pools
    - handle both IPV4 and IPV6
    - three cases to test
        - user provides allocation_pools the same as the default [5:-2]
        - user provides allocation_pools smaller than the default
        - user provides allocation_pools larger than the default
    
    RM4825

--- tests/test_default_ip_policy.py
+++ tests/test_default_ip_policy.py
@@ -12,30 +12,36 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
-import contextlib
+import json
 import mock
 from mock import patch
 from tests import test_base
 
 from wafflehaus.neutron.ip_policy import create_default
 
+class DefaultPolicyTestBase(test_base.TestBase):
+    def _get_allocation_pools_from_body(self, body):
+        body_json = json.loads(body)
+        print(body_json)
+        allocation_pools = body_json["subnets"][0]["allocation_pools"]
+        return allocation_pools
 
-class TestDefaultIPPolicy(test_base.TestBase):
+class TestDefaultIPV4Policy(DefaultPolicyTestBase):
     def setUp(self):
         self.app = mock.Mock()
 
-        self.add_mock = mock.Mock()
+        self.default_mock = mock.Mock()
         self.mod_mock = mock.Mock()
 
-        self.v4_body = ('{ "subnets":[' +
-                        '{' +
-                        '"cidr":"192.168.199.0/24",' +
-                        '"ip_version":4,' +
-                        '"network_id":"some_id"' +
-                        '}' +
-                        '] }')
+        self.v4_no_alloc = ('{ "subnets":[' +
+                            '{' +
+                            '"cidr":"192.168.199.0/24",' +
+                            '"ip_version":4,' +
+                            '"network_id":"some_id"' +
+                            '}' +
+                            '] }')
 
-        self.v4_has_body = ('{ "subnets":[' +
+        self.v4_has_alloc_same_as_default = ('{ "subnets":[' +
                             '{' +
                             '"cidr":"192.168.199.0/24",' +
                             '"ip_version":4,' +
@@ -43,6 +49,30 @@ class TestDefaultIPPolicy(test_base.TestBase):
                             '"allocation_pools":[' +
                             '{ "end":"192.168.199.254", ' +
                             '"start":"192.168.199.5" }' +
+                            ']' +
+                            '}' +
+                            '] }')
+        
+        self.v4_has_alloc_bigger_than_default = ('{ "subnets":[' +
+                            '{' +
+                            '"cidr":"192.168.199.0/24",' +
+                            '"ip_version":4,' +
+                            '"network_id":"some_id",' +
+                            '"allocation_pools":[' +
+                            '{ "end":"192.168.199.255", ' +
+                            '"start":"192.168.199.0" }' +
+                            ']' +
+                            '}' +
+                            '] }')
+        
+        self.v4_has_alloc_smaller_than_default = ('{ "subnets":[' +
+                            '{' +
+                            '"cidr":"192.168.199.0/24",' +
+                            '"ip_version":4,' +
+                            '"network_id":"some_id",' +
+                            '"allocation_pools":[' +
+                            '{ "end":"192.168.199.100", ' +
+                            '"start":"192.168.199.85" }' +
                             ']' +
                             '}' +
                             '] }')
@@ -57,64 +87,206 @@ class TestDefaultIPPolicy(test_base.TestBase):
         conf = {'resource': 'POST /derp'}
         result = create_default.filter_factory(conf)(self.app)
         pkg = 'wafflehaus.neutron.ip_policy.create_default.DefaultIPPolicy'
-        with patch(pkg + '._filter_policy', self.add_mock) as mock:
+        with patch(pkg + '._filter_policy', self.default_mock) as mock:
             result.__call__.request('/derp', method='POST')
             self.assertTrue(mock.called)
 
     def test_matched_default(self):
         result = create_default.filter_factory({})(self.app)
         pkg = 'wafflehaus.neutron.ip_policy.create_default.DefaultIPPolicy'
-        with patch(pkg + '._filter_policy', self.add_mock) as mock:
+        with patch(pkg + '._filter_policy', self.default_mock) as mock:
             result.__call__.request('/v2.0/subnets', method='POST')
             self.assertTrue(mock.called)
 
     def test_not_matched_default(self):
         result = create_default.filter_factory({})(self.app)
         pkg = 'wafflehaus.neutron.ip_policy.create_default.DefaultIPPolicy'
-        with patch(pkg + '._filter_policy', self.add_mock) as mock:
+        with patch(pkg + '._filter_policy', self.default_mock) as mock:
             result.__call__.request('/v2.0/subnets', method='GET')
             self.assertFalse(mock.called)
 
     def test_not_matched(self):
         result = create_default.filter_factory({})(self.app)
         pkg = 'wafflehaus.neutron.ip_policy.create_default.DefaultIPPolicy'
-        with patch(pkg + '._filter_policy', self.add_mock) as mock:
+        with patch(pkg + '._filter_policy', self.default_mock) as mock:
             result.__call__.request('/testing', method='POST')
             self.assertFalse(mock.called)
 
-    @contextlib.contextmanager
-    def test_add_called(self):
+    def test_body_contains_no_allocation_pools(self):
         result = create_default.filter_factory({})(self.app)
-        body = self.v4_body
-        pkg = 'wafflehaus.neutron.ip_policy.create_default.DefaultIPPolicy'
-        with contextlib.nested(
-            mock.patch(pkg + '._add_allocation_pools'),
-            mock.patch(pkg + '._modify_allocation_pools')
-        ) as (add, mod):
-            result.__call__.request('/v2.0/subnets', method='POST',
-                                    body=body)
-            self.assertTrue(add.called)
-            self.assertFalse(mod.called)
-
-    @contextlib.contextmanager
-    def test_mod_called(self):
-        result = create_default.filter_factory({})(self.app)
-        body = self.v4_has_body
-        pkg = 'wafflehaus.neutron.ip_policy.create_default.DefaultIPPolicy'
-        with contextlib.nested(
-            mock.patch(pkg + '._add_allocation_pools'),
-            mock.patch(pkg + '._modify_allocation_pools')
-        ) as (add, mod):
-            result.__call__.request('/v2.0/subnets', method='POST',
-                                    body=body)
-            self.assertFalse(add.called)
-            self.assertTrue(mod.called)
-
-    def test_body_modified(self):
-        self.skipTest("Just for now")
-        result = create_default.filter_factory({})(self.app)
-        body = self.v4_body
+        body = self.v4_no_alloc
         resp = result.__call__.request('/v2.0/subnets', method='POST',
                                        body=body)
         self.assertTrue(200, resp.status_code)
         self.assertNotEqual(result.body, body)
+        allocation_pools= self._get_allocation_pools_from_body(result.body)
+        self.assertEqual("192.168.199.5", allocation_pools[0]["start"])
+        self.assertEqual("192.168.199.254", allocation_pools[0]["end"])
+    
+    def test_body_contains_allocation_pools_same_as_default(self):
+        result = create_default.filter_factory({})(self.app)
+        body = self.v4_has_alloc_same_as_default
+        resp = result.__call__.request('/v2.0/subnets', method='POST',
+                                       body=body)
+        self.assertTrue(200, resp.status_code)
+        self.assertNotEqual(result.body, body)
+        allocation_pools = self._get_allocation_pools_from_body(result.body)
+        self.assertEqual("192.168.199.5", allocation_pools[0]["start"])
+        self.assertEqual("192.168.199.254", allocation_pools[0]["end"])
+    
+    def test_body_contains_allocation_pool_bigger_than_default(self):
+        result = create_default.filter_factory({})(self.app)
+        body = self.v4_has_alloc_bigger_than_default
+        resp = result.__call__.request('/v2.0/subnets', method='POST',
+                                       body=body)
+        self.assertTrue(200, resp.status_code)
+        self.assertNotEqual(result.body, body)
+        allocation_pools = self._get_allocation_pools_from_body(result.body)
+        self.assertEqual("192.168.199.5", allocation_pools[0]["start"])
+        self.assertEqual("192.168.199.254", allocation_pools[0]["end"])
+
+    def test_body_contains_allocation_pool_smaller_than_default(self):
+        result = create_default.filter_factory({})(self.app)
+        body = self.v4_has_alloc_smaller_than_default
+        resp = result.__call__.request('/v2.0/subnets', method='POST',
+                                       body=body)
+        self.assertTrue(200, resp.status_code)
+        self.assertNotEqual(result.body, body)
+        allocation_pools = self._get_allocation_pools_from_body(result.body)
+        self.assertEqual("192.168.199.85", allocation_pools[0]["start"])
+        self.assertEqual("192.168.199.100", allocation_pools[0]["end"])
+
+
+class TestDefaultIPV6Policy(DefaultPolicyTestBase):
+    def setUp(self):
+        self.app = mock.Mock()
+
+        self.default_mock = mock.Mock()
+        self.mod_mock = mock.Mock()
+
+        self.v6_no_alloc = ('{ "subnets":[' +
+                            '{' +
+                            '"cidr":"2607:f0d0:1002:51::0/96",' +
+                            '"ip_version":6,' +
+                            '"network_id":"some_id"' +
+                            '}' +
+                            '] }')
+
+        self.v6_has_alloc_same_as_default = ('{ "subnets":[' +
+                       '{' +
+                       '"cidr":"2607:f0d0:1002:51::0/96",' +
+                       '"ip_version":6,' +
+                       '"network_id":"some_id",' +
+                       '"allocation_pools":[' +
+                       '{ "end":"2607:f0d0:1002:51::ffff:fffe", ' +
+                       '"start":"2607:f0d0:1002:51::a" }' +
+                       ']' +
+                       '}' +
+                       '] }')
+        
+        self.v6_has_alloc_bigger_than_default = ('{ "subnets":[' +
+                       '{' +
+                       '"cidr":"2607:f0d0:1002:51::0/96",' +
+                       '"ip_version":6,' +
+                       '"network_id":"some_id",' +
+                       '"allocation_pools":[' +
+                       '{ "end":"2607:f0d0:1002:51::ffff:ffff", ' +
+                       '"start":"2607:f0d0:1002:51::" }' +
+                       ']' +
+                       '}' +
+                       '] }')
+        
+        self.v6_has_alloc_smaller_than_default = ('{ "subnets":[' +
+                            '{' +
+                            '"cidr":"2607:f0d0:1002:51::0/96",' +
+                            '"ip_version":6,' +
+                            '"network_id":"some_id",' +
+                            '"allocation_pools":[' +
+                            '{ "end":"2607:f0d0:1002:51::64", ' +
+                            '"start":"2607:f0d0:1002:51::56" }' +
+                            ']' +
+                            '}' +
+                            '] }')
+
+    def test_default_instance_create(self):
+        result = create_default.filter_factory({})(self.app)
+        self.assertIsNotNone(result)
+        resp = result.__call__.request('/', method='GET')
+        self.assertEqual(resp, self.app)
+
+    def test_matched_configured(self):
+        conf = {'resource': 'POST /derp'}
+        result = create_default.filter_factory(conf)(self.app)
+        pkg = 'wafflehaus.neutron.ip_policy.create_default.DefaultIPPolicy'
+        with patch(pkg + '._filter_policy', self.default_mock) as mock:
+            result.__call__.request('/derp', method='POST')
+            self.assertTrue(mock.called)
+
+    def test_matched_default(self):
+        result = create_default.filter_factory({})(self.app)
+        pkg = 'wafflehaus.neutron.ip_policy.create_default.DefaultIPPolicy'
+        with patch(pkg + '._filter_policy', self.default_mock) as mock:
+            result.__call__.request('/v2.0/subnets', method='POST')
+            self.assertTrue(mock.called)
+
+    def test_not_matched_default(self):
+        result = create_default.filter_factory({})(self.app)
+        pkg = 'wafflehaus.neutron.ip_policy.create_default.DefaultIPPolicy'
+        with patch(pkg + '._filter_policy', self.default_mock) as mock:
+            result.__call__.request('/v2.0/subnets', method='GET')
+            self.assertFalse(mock.called)
+
+    def test_not_matched(self):
+        result = create_default.filter_factory({})(self.app)
+        pkg = 'wafflehaus.neutron.ip_policy.create_default.DefaultIPPolicy'
+        with patch(pkg + '._filter_policy', self.default_mock) as mock:
+            result.__call__.request('/testing', method='POST')
+            self.assertFalse(mock.called)
+
+    def test_body_contains_no_allocation_pools(self):
+        result = create_default.filter_factory({})(self.app)
+        body = self.v6_no_alloc
+        resp = result.__call__.request('/v2.0/subnets', method='POST',
+                                       body=body)
+        self.assertTrue(200, resp.status_code)
+        self.assertNotEqual(result.body, body)
+        allocation_pools = self._get_allocation_pools_from_body(result.body)
+        self.assertEqual("2607:f0d0:1002:51::a", allocation_pools[0]["start"])
+        self.assertEqual("2607:f0d0:1002:51::ffff:fffe",
+                         allocation_pools[0]["end"])
+    
+    def test_body_contains_allocation_pools_same_as_default(self):
+        result = create_default.filter_factory({})(self.app)
+        body = self.v6_has_alloc_same_as_default
+        resp = result.__call__.request('/v2.0/subnets', method='POST',
+                                       body=body)
+        self.assertTrue(200, resp.status_code)
+        self.assertNotEqual(result.body, body)
+        allocation_pools = self._get_allocation_pools_from_body(result.body)
+        self.assertEqual("2607:f0d0:1002:51::a", allocation_pools[0]["start"])
+        self.assertEqual("2607:f0d0:1002:51::ffff:fffe",
+                         allocation_pools[0]["end"])
+    
+    def test_body_contains_allocation_pool_bigger_than_default(self):
+        result = create_default.filter_factory({})(self.app)
+        body = self.v6_has_alloc_bigger_than_default
+        resp = result.__call__.request('/v2.0/subnets', method='POST',
+                                       body=body)
+        self.assertTrue(200, resp.status_code)
+        self.assertNotEqual(result.body, body)
+        allocation_pools = self._get_allocation_pools_from_body(result.body)
+        self.assertEqual("2607:f0d0:1002:51::a", allocation_pools[0]["start"])
+        self.assertEqual("2607:f0d0:1002:51::ffff:fffe",
+                         allocation_pools[0]["end"])
+
+    def test_body_contains_allocation_pool_smaller_than_default(self):
+        result = create_default.filter_factory({})(self.app)
+        body = self.v6_has_alloc_smaller_than_default
+        resp = result.__call__.request('/v2.0/subnets', method='POST',
+                                       body=body)
+        self.assertTrue(200, resp.status_code)
+        self.assertNotEqual(result.body, body)
+        allocation_pools = self._get_allocation_pools_from_body(result.body)
+        self.assertEqual("2607:f0d0:1002:51::56", allocation_pools[0]["start"])
+        self.assertEqual("2607:f0d0:1002:51::64",
+                         allocation_pools[0]["end"])
--- wafflehaus/neutron/ip_policy/create_default.py
+++ wafflehaus/neutron/ip_policy/create_default.py
@@ -15,6 +15,7 @@
 
 import json
 import logging
+import netaddr
 import webob.dec
 import webob.exc
 
@@ -34,11 +35,52 @@ class DefaultIPPolicy(object):
         self.resource = conf.get('resource', 'POST /v2.0/subnets')
         self.resources = rf.parse_resources(self.resource)
 
-    def _add_allocation_pools(self, req):
-        pass
+    def _pools_from_ipset(self, ipset):
+        cidrs = ipset.iter_cidrs()
+        if len(cidrs) == 0:
+            return []
+        if len(cidrs) == 1:
+            return [dict(start=str(cidrs[0][0]),
+                         end=str(cidrs[0][-1]))]
 
-    def _modify_allocation_pools(self, req):
-        self.log.info("derp")
+        pool_start = cidrs[0][0]
+        prev_cidr_end = cidrs[0][-1]
+        pools = []
+        for cidr in cidrs[1:]:
+            cidr_start = cidr[0]
+            if prev_cidr_end + 1 != cidr_start:
+                pools.append(dict(start=str(pool_start),
+                                  end=str(prev_cidr_end)))
+                pool_start = cidr_start
+            prev_cidr_end = cidr[-1]
+        pools.append(dict(start=str(pool_start), end=str(prev_cidr_end)))
+        return pools
+
+    def _get_default_allocation_pools(self, subnet):
+        alloc_pools = {}
+        cidr_net = netaddr.IPNetwork(subnet["cidr"])
+        starting_index = 5 if subnet.get("ip_version") == 4 else 10
+        start = cidr_net[starting_index]
+        end = cidr_net[-2]
+        alloc_pools = [{"start": str(start),
+                       "end": str(end)}]
+        return alloc_pools
+
+    def _modify_allocation_pools(self, subnet):
+        alloc_pools = subnet.get('allocation_pools')
+        default_alloc_pools = self._get_default_allocation_pools(subnet)
+        default_start = netaddr.IPAddress(default_alloc_pools[0]["start"])
+        default_end = netaddr.IPAddress(default_alloc_pools[0]["end"])
+        default_set = \
+            netaddr.IPSet(netaddr.IPRange(default_start, default_end).cidrs())
+        for p in alloc_pools:
+            start = netaddr.IPAddress(p["start"])
+            end = netaddr.IPAddress(p["end"])
+            default_set &= netaddr.IPSet(
+                netaddr.IPRange(
+                    netaddr.IPAddress(start), netaddr.IPAddress(end)).cidrs())
+        alloc_pools = self._pools_from_ipset(default_set)
+        return alloc_pools
 
     def _filter_policy(self, req):
         body = req.body
@@ -47,15 +89,18 @@ class DefaultIPPolicy(object):
         except ValueError:
             return webob.exc.HTTPBadRequest
         subnets = body_json.get('subnets')
+        body_json["subnets"] = []
         if subnets is None:
             return self.app
         for subnet in subnets:
             alloc_pools = subnet.get('allocation_pools')
             if alloc_pools is None:
-                self._add_allocation_pools(req)
+                alloc_pools = self._get_default_allocation_pools(subnet)
             else:
-                self._modify_allocation_pools(req)
-        self.body = body
+                alloc_pools = self._modify_allocation_pools(subnet)
+            subnet["allocation_pools"] = alloc_pools
+            body_json["subnets"].append(subnet)
+        self.body = json.dumps(body_json)
         return self.app
 
     @webob.dec.wsgify
