roaet/wafflehaus_neutron

JIRA:NCP-2223 Logging changes for wafflehaus neutron

wafflehaus/neutron/ip_policy/create_default.py
@@ -28,12 +28,12 @@ class DefaultIPPolicy(WafflehausBase):
     def __init__(self, app, conf):
         super(DefaultIPPolicy, self).__init__(app, conf)
         self.log.name = conf.get('log_name', __name__)
-        self.log.info('Starting wafflehaus default ip policy middleware')
         self.resource = conf.get('resource', 'POST /v2.0/subnets')
         self.resources = rf.parse_resources(self.resource)
 
     def _pools_from_ipset(self, ipset):
         cidrs = ipset.iter_cidrs()
+        self.log.info(' _pools_from_ipset - CIDRS -> ' + str(cidrs))
         if len(cidrs) == 0:
             return []
         if len(cidrs) == 1:
@@ -51,6 +51,8 @@ class DefaultIPPolicy(WafflehausBase):
                 pool_start = cidr_start
             prev_cidr_end = cidr[-1]
         pools.append(dict(start=str(pool_start), end=str(prev_cidr_end)))
+        self.log.info(' _pools_from_ipset - Pools from ip set : ' +
+                      str(pools))
         return pools
 
     def _get_default_allocation_pools(self, subnet):
@@ -66,6 +68,8 @@ class DefaultIPPolicy(WafflehausBase):
     def _modify_allocation_pools(self, subnet):
         alloc_pools = subnet.get('allocation_pools')
         default_alloc_pools = self._get_default_allocation_pools(subnet)
+        self.log.info(' _get_default_allocation_pools - '
+                      'Default allocation pool -> ' + str(default_alloc_pools))
         default_start = netaddr.IPAddress(default_alloc_pools[0]["start"])
         default_end = netaddr.IPAddress(default_alloc_pools[0]["end"])
         default_set = netaddr.IPSet(netaddr.IPRange(default_start,
@@ -82,28 +86,69 @@ class DefaultIPPolicy(WafflehausBase):
         return alloc_pools
 
     def _filter_policy(self, req):
+        # req.environ['neutron.context'].__dict__) - This is all context var
+        context_dict = req.environ.get('neutron.context')
+        if context_dict:
+            self.log.info(' _check_basics , Neutron Context : ' +
+                          'request id:%s , project id :%s ,'
+                          'tenant name %s , is admin %s ,user id %s '
+                          % (str(context_dict.request_id),
+                             str(context_dict.project_id),
+                             str(context_dict.tenant_name),
+                             str(context_dict.is_admin),
+                             str(context_dict.user_id)))
+        tenant_id = req.headers.get('X_TENANT_ID')
+        user_id = req.headers.get('X_USER_ID')
         body = req.body
+        self.log.info(' _filter_policy - '
+                      'Filter policy request body -> '
+                      '%s tenant_id %s and '
+                      'user_id %s ' % (str(body), tenant_id, user_id))
         try:
             body_json = json.loads(body)
         except ValueError:
+            self.log.error(' _filter_policy - '
+                           'Cloud not load request body as json '
+                           'while filtering policy,check if the json is valid '
+                           '%s tenant_id %s and '
+                           'user_id %s ' % (str(body), tenant_id, user_id))
             return webob.exc.HTTPBadRequest
         subnets = body_json.get('subnets')
         subnet = body_json.get('subnet')
+        self.log.info(' _filter_policy - Subnets -> '
+                      '%s tenant_id %s and '
+                      'user_id %s ' % (str(subnets), tenant_id, user_id))
+        self.log.info(' _filter_policy - Subnet -> ' + str(subnet))
         body_json["subnets"] = []
         if subnets is None and subnet is None:
+            self.log.debug(' _filter_policy - Both subnets and '
+                           'subnet is None in request json bodyi '
+                           'tenant_id %s and user_id %s ' %
+                           (tenant_id, user_id))
             """If this is true there is nothing to work with let app error."""
             return self.app
         single = False
         if subnets is None:
             """If this is true then it's a single, put it in list."""
+            self.log.debug(' _filter_policy - Subnets is None but '
+                           'subnet is not, means it is single '
+                           'tenant_id %s and user_id %s ' %
+                           (tenant_id, user_id))
             single = True
             subnets = [subnet]
         for subnet in subnets:
             alloc_pools = subnet.get('allocation_pools')
             if alloc_pools is None:
+                self.log.debug(' _filter_policy - Allocation pools is '
+                               'None , getting default allocation pools '
+                               'tenant_id %s and user_id %s ' %
+                               (tenant_id, user_id))
                 alloc_pools = self._get_default_allocation_pools(subnet)
             else:
                 alloc_pools = self._modify_allocation_pools(subnet)
+                self.log.info(' _modify_allocation_pools - '
+                              'Modified allocation pool -> ' + str(
+                                  alloc_pools))
             subnet["allocation_pools"] = alloc_pools
             body_json["subnets"].append(subnet)
         if single:

wafflehaus/neutron/last_ip_check/last_ip_check.py
@@ -25,21 +25,51 @@ class LastIpCheck(WafflehausBase):
     def __init__(self, app, conf):
         super(LastIpCheck, self).__init__(app, conf)
         self.log.name = conf.get('log_name', __name__)
-        self.log.info('Starting wafflehaus last_ip_check middleware')
 
     def _check_basics(self, req):
+        # req.environ['neutron.context'].__dict__) - This is all context var
+        context_dict = req.environ.get('neutron.context')
+        if context_dict:
+            self.log.info(' _check_basics , Neutron Context : ' +
+                          'request id:%s , project id :%s ,'
+                          'tenant name %s , is admin %s ,user id %s '
+                          % (str(context_dict.request_id),
+                             str(context_dict.project_id),
+                             str(context_dict.tenant_name),
+                             str(context_dict.is_admin),
+                             str(context_dict.user_id)))
+        tenant_id = req.headers.get('X_TENANT_ID')
+        user_id = req.headers.get('X_USER_ID')
+        self.log.info(' _check_basics - '
+                      'tenant_id %s and user_id'
+                      ' %s' % (tenant_id, user_id))
         if req.content_length == 0:
+            self.log.debug(' _check_basics - '
+                           'Content length of request is zero '
+                           '_check_basics returned False for module '
+                           'tenant_id %s and user_id'
+                           ' %s' % (tenant_id, user_id))
             return False
         method = req.method
         if method not in ['PUT']:
+            self.log.debug(' _check_basics - PUT not in request method '
+                           'tenant_id %s and user_id'
+                           ' %s' % (tenant_id, user_id))
             return False
         url = req.path
         url_parts = url.split('/')
         if 'ports' not in url and 'ports' not in url_parts[len(url_parts) - 2]:
+            self.log.debug(' _check_basics - Port '
+                           'not found in url, or url_parts[len('
+                           'url_parts) _check_basics retuned False'
+                           'tenant_id %s and user_id'
+                           ' %s' % (tenant_id, user_id))
             return False
         return True
 
     def _should_run(self, req):
+        tenant_id = req.headers.get('X_TENANT_ID')
+        user_id = req.headers.get('X_USER_ID')
         basic_check = self._check_basics(req)
         if isinstance(basic_check, webob.exc.HTTPException) or not basic_check:
             return basic_check
@@ -47,25 +77,43 @@ class LastIpCheck(WafflehausBase):
         try:
             body_json = json.loads(body)
         except ValueError:
+            self.log.error(' _should_run - Failed while loading json,'
+                           'check for invalid json tenant_id %s and user_id'
+                           ' %s' % (tenant_id, user_id))
             return webob.exc.HTTPBadRequest
         try:
             port_info = body_json.get('port')
         except AttributeError:
+            self.log.error(' _should_run - Port not found in '
+                           'request body json tenant_id %s and user_id'
+                           ' %s' % (tenant_id, user_id))
             return False
         if port_info is None:
+            self.log.debug(' _should_run - Port info is None '
+                           'in the request body json tenant_id %s and user_id'
+                           ' %s' % (tenant_id, user_id))
             return False
         fixed_ips = port_info.get('fixed_ips')
         if fixed_ips is None:
+            self.log.debug(' _should_run - Fixed IP is None '
+                           'in request body json tenant_id %s and user_id'
+                           ' %s' % (tenant_id, user_id))
             return False
         self.fixed_ips = fixed_ips
         return True
 
     def _is_last_ip(self, req):
+        tenant_id = req.headers.get('X_TENANT_ID')
+        user_id = req.headers.get('X_USER_ID')
+        self.log.info(' _is_last_ip - Checking if the attached IP is '
+                      'the last address tenant_id %s and user_id %s' %
+                      (tenant_id, user_id))
         if not hasattr(self, 'fixed_ips') or self.fixed_ips is None:
             return self.app
         if len(self.fixed_ips) == 0:
-            self.log.error('PUT requests to remove all IPs from a Port '
-                           'are not allowed')
+            self.log.error(' _is_last_ip - PUT requests to remove all '
+                           'IPs from a Port are not allowed tenant_id %s '
+                           ' and user_id %s' % (tenant_id, user_id))
             return webob.exc.HTTPForbidden("fixed_ips cannot be empty")
         else:
             return self.app

roaet/wafflehaus.neutron

wafflehaus/neutron/ip_policy/create_default.py
@@ -28,12 +28,12 @@ class DefaultIPPolicy(WafflehausBase):
     def __init__(self, app, conf):
         super(DefaultIPPolicy, self).__init__(app, conf)
         self.log.name = conf.get('log_name', __name__)
-        self.log.info('Starting wafflehaus default ip policy middleware')
         self.resource = conf.get('resource', 'POST /v2.0/subnets')
         self.resources = rf.parse_resources(self.resource)
 
     def _pools_from_ipset(self, ipset):
         cidrs = ipset.iter_cidrs()
+        self.log.info('_pools_from_ipset - CIDRS -> ' + str(cidrs))
         if len(cidrs) == 0:
             return []
         if len(cidrs) == 1:
@@ -51,6 +51,8 @@ class DefaultIPPolicy(WafflehausBase):
                 pool_start = cidr_start
             prev_cidr_end = cidr[-1]
         pools.append(dict(start=str(pool_start), end=str(prev_cidr_end)))
+        self.log.info('_pools_from_ipset - Pools from ip set -> ' +
+                      str(pools))
         return pools
 
     def _get_default_allocation_pools(self, subnet):
@@ -66,6 +68,8 @@ class DefaultIPPolicy(WafflehausBase):
     def _modify_allocation_pools(self, subnet):
         alloc_pools = subnet.get('allocation_pools')
         default_alloc_pools = self._get_default_allocation_pools(subnet)
+        self.log.info('_get_default_allocation_pools - '
+                      'Default allocation pool -> ' + str(default_alloc_pools))
         default_start = netaddr.IPAddress(default_alloc_pools[0]["start"])
         default_end = netaddr.IPAddress(default_alloc_pools[0]["end"])
         default_set = netaddr.IPSet(netaddr.IPRange(default_start,
@@ -82,28 +86,68 @@ class DefaultIPPolicy(WafflehausBase):
         return alloc_pools
 
     def _filter_policy(self, req):
+        context_dict = req.environ.get('neutron.context')
+        if context_dict:
+            self.log.info('_filter_policy, Neutron Context ' +
+                          'request id %s, project id %s, '
+                          'tenant name %s, is admin %s, user id %s'
+                          % (str(context_dict.request_id),
+                             str(context_dict.project_id),
+                             str(context_dict.tenant_name),
+                             str(context_dict.is_admin),
+                             str(context_dict.user_id)))
+        tenant_id = req.headers.get('X_TENANT_ID')
+        user_id = req.headers.get('X_USER_ID')
         body = req.body
+        self.log.info('_filter_policy - '
+                      'Filter policy request body -> '
+                      '%s tenant_id %s and '
+                      'user_id %s' % (str(body), tenant_id, user_id))
         try:
             body_json = json.loads(body)
         except ValueError:
+            self.log.error('_filter_policy - '
+                           'Could not load request body as json '
+                           'while filtering policy, check if the json is valid'
+                           ' %s tenant_id %s and '
+                           'user_id %s' % (str(body), tenant_id, user_id))
             return webob.exc.HTTPBadRequest
         subnets = body_json.get('subnets')
         subnet = body_json.get('subnet')
+        self.log.info('_filter_policy - Subnets -> '
+                      '%s tenant_id %s and '
+                      'user_id %s' % (str(subnets), tenant_id, user_id))
+        self.log.info(' _filter_policy - Subnet -> ' + str(subnet))
         body_json["subnets"] = []
         if subnets is None and subnet is None:
+            self.log.debug('_filter_policy - Both subnets and '
+                           'subnet is None in request json body '
+                           'tenant_id %s and user_id %s' %
+                           (tenant_id, user_id))
             """If this is true there is nothing to work with let app error."""
             return self.app
         single = False
         if subnets is None:
             """If this is true then it's a single, put it in list."""
+            self.log.debug('_filter_policy - Subnets is None but '
+                           'subnet is not, means it is single '
+                           'tenant_id %s and user_id %s' %
+                           (tenant_id, user_id))
             single = True
             subnets = [subnet]
         for subnet in subnets:
             alloc_pools = subnet.get('allocation_pools')
             if alloc_pools is None:
+                self.log.debug('_filter_policy - Allocation pools is '
+                               'None, getting default allocation pools '
+                               'tenant_id %s and user_id %s' %
+                               (tenant_id, user_id))
                 alloc_pools = self._get_default_allocation_pools(subnet)
             else:
                 alloc_pools = self._modify_allocation_pools(subnet)
+                self.log.info('_modify_allocation_pools - '
+                              'Modified allocation pool -> ' + str(
+                                  alloc_pools))
             subnet["allocation_pools"] = alloc_pools
             body_json["subnets"].append(subnet)
         if single:

wafflehaus/neutron/last_ip_check/last_ip_check.py
@@ -25,21 +25,50 @@ class LastIpCheck(WafflehausBase):
     def __init__(self, app, conf):
         super(LastIpCheck, self).__init__(app, conf)
         self.log.name = conf.get('log_name', __name__)
-        self.log.info('Starting wafflehaus last_ip_check middleware')
 
     def _check_basics(self, req):
+        context_dict = req.environ.get('neutron.context')
+        if context_dict:
+            self.log.info('_check_basics - Neutron Context ' +
+                          'request id %s, project id %s, '
+                          'tenant name %s, is admin %s, user id %s'
+                          % (str(context_dict.request_id),
+                             str(context_dict.project_id),
+                             str(context_dict.tenant_name),
+                             str(context_dict.is_admin),
+                             str(context_dict.user_id)))
+        tenant_id = req.headers.get('X_TENANT_ID')
+        user_id = req.headers.get('X_USER_ID')
+        self.log.info('_check_basics - '
+                      'tenant_id %s and user_id '
+                      '%s' % (tenant_id, user_id))
         if req.content_length == 0:
+            self.log.debug('_check_basics - '
+                           'Content length of request is zero '
+                           '_check_basics returned False for module '
+                           'tenant_id %s and user_id '
+                           '%s' % (tenant_id, user_id))
             return False
         method = req.method
         if method not in ['PUT']:
+            self.log.debug('_check_basics - PUT not in request method '
+                           'tenant_id %s and user_id '
+                           '%s' % (tenant_id, user_id))
             return False
         url = req.path
         url_parts = url.split('/')
         if 'ports' not in url and 'ports' not in url_parts[len(url_parts) - 2]:
+            self.log.debug('_check_basics - Port '
+                           'not found in url, or url_parts[len('
+                           'url_parts) _check_basics retuned False '
+                           'tenant_id %s and user_id '
+                           '%s' % (tenant_id, user_id))
             return False
         return True
 
     def _should_run(self, req):
+        tenant_id = req.headers.get('X_TENANT_ID')
+        user_id = req.headers.get('X_USER_ID')
         basic_check = self._check_basics(req)
         if isinstance(basic_check, webob.exc.HTTPException) or not basic_check:
             return basic_check
@@ -47,25 +76,43 @@ class LastIpCheck(WafflehausBase):
         try:
             body_json = json.loads(body)
         except ValueError:
+            self.log.error('_should_run - Failed while loading json, '
+                           'check for invalid json tenant_id %s and user_id '
+                           '%s' % (tenant_id, user_id))
             return webob.exc.HTTPBadRequest
         try:
             port_info = body_json.get('port')
         except AttributeError:
+            self.log.error('_should_run - Port not found in '
+                           'request body json tenant_id %s and user_id '
+                           '%s' % (tenant_id, user_id))
             return False
         if port_info is None:
+            self.log.debug('_should_run - Port info is None '
+                           'in the request body json tenant_id %s and user_id '
+                           '%s' % (tenant_id, user_id))
             return False
         fixed_ips = port_info.get('fixed_ips')
         if fixed_ips is None:
+            self.log.debug('_should_run - Fixed IP is None '
+                           'in request body json tenant_id %s and user_id '
+                           '%s' % (tenant_id, user_id))
             return False
         self.fixed_ips = fixed_ips
         return True
 
     def _is_last_ip(self, req):
+        tenant_id = req.headers.get('X_TENANT_ID')
+        user_id = req.headers.get('X_USER_ID')
+        self.log.info('_is_last_ip - Checking if the attached IP is '
+                      'the last address tenant_id %s and user_id %s' %
+                      (tenant_id, user_id))
         if not hasattr(self, 'fixed_ips') or self.fixed_ips is None:
             return self.app
         if len(self.fixed_ips) == 0:
-            self.log.error('PUT requests to remove all IPs from a Port '
-                           'are not allowed')
+            self.log.error('_is_last_ip - PUT requests to remove all '
+                           'IPs from a Port are not allowed tenant_id %s '
+                           'and user_id %s' % (tenant_id, user_id))
             return webob.exc.HTTPForbidden("fixed_ips cannot be empty")
         else:
             return self.app